b92c8fc6df1bc8c7dc4416e8f2eab0d8ac2c57ac
maxeboykin <maxericboykin@gmail.com>
2023-10-28 16:03:19 -0400

    first commit

---
+++ 9-statements/rewrite-for-to-while.cpp
@@ -0,0 +1,17 @@
+/*
+ *  Rewrite the following for-statement as an equivalent while-statement
+*/
+#include <iostream>
+
+int main(){
+
+    for(int i = 0; i != max_length; i++){
+        if(input_line[i] == '?'){
+            quest_count++;
+        }
+    }
+
+
+
+}
+
